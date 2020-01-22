948cbd4f7575cb9ed38c6c5ac9d2c7eb0b32b2ae
GregHib <gregoryhib@gmail.com>
2020-01-22 09:40:05 +0000

    Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.3.61"
+}
+
+group = "world.gregs.game.ai.playground"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
