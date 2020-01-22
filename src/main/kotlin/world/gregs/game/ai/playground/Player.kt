148c24d60b884869aac55d7afc78258f89ebcdb6
Greg <gregoryhib@gmail.com>
2020-01-22 09:45:05 +0000

    WIP

---
+++ src/main/kotlin/world/gregs/game/ai/playground/Player.kt
@@ -0,0 +1,35 @@
+package world.gregs.game.ai.playground
+
+/*
+
+    Waypoint map
+        Each waypoint link has a base weight (tiles to destination or heuristic) plus a value calculated for each individual entity to allow for requirements (quest completed, level requirements, 30min cooldown by multiply by 0), or cost (100gp * entity characteristics)
+
+    What about resting/listening to music? (If link intersects radius of musician then decrease weight?)
+
+    Path/route
+    1) Decide on a target
+    2) Traverse to that target
+        Considerations:
+            Shortcuts (weighted)
+            Large map
+            Multiple entities
+            Fixed targets
+    Traversal
+        Obstacles
+            Doors
+            Gates
+            Agility shortcuts
+        Methods
+            Walk
+            Run
+            Tele
+            Boat
+        Targets
+            Bank
+            Activity location
+                Skill
+                Quest
+                etc...
+            Shop
+ */
---
+++ src/main/kotlin/world/gregs/game/ai/playground/RegionGrid.kt
@@ -0,0 +1,73 @@
+package world.gregs.game.ai.playground
+
+import java.awt.*
+import javax.swing.JFrame
+import javax.swing.JPanel
+import javax.swing.UIManager
+import javax.swing.UnsupportedLookAndFeelException
+import kotlin.math.max
+import kotlin.random.Random
+
+class RegionGrid {
+    inner class GridPane(val data: Array<Array<Color?>>) : JPanel() {
+        override fun getPreferredSize(): Dimension {
+            return Dimension(900, 900)
+        }
+
+        override fun paintComponent(g: Graphics) {
+            super.paintComponent(g)
+            val g2d = g.create() as Graphics2D
+
+            val size = max(1, height/data[0].size)
+            //Fill
+            for(x in data.indices) {
+                for(y in data[0].indices) {
+                    if(data[x][y] != null) {
+                        g.color = data[x][y]
+                        g.fillRect(x * size + (width - size * data.size) / 2, y * size, size, size)
+                    }
+                }
+            }
+            g2d.dispose()
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            RegionGrid()
+        }
+    }
+
+    init {
+        EventQueue.invokeLater {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+            } catch (ex: ClassNotFoundException) {
+                ex.printStackTrace()
+            } catch (ex: InstantiationException) {
+                ex.printStackTrace()
+            } catch (ex: IllegalAccessException) {
+                ex.printStackTrace()
+            } catch (ex: UnsupportedLookAndFeelException) {
+                ex.printStackTrace()
+            }
+            val frame = JFrame("Testing")
+            frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+            val grid = Array(800) { Array<Color?>(800) { null } }
+            for(x in grid.indices) {
+                for(y in grid[0].indices) {
+                    if(Random.nextDouble() < 0.15) {
+                        grid[x][y] = Color.BLACK
+                    }
+                }
+            }
+            grid[5][5] = Color.BLUE
+            grid[6][5] = Color.BLUE
+            frame.add(GridPane(grid))
+            frame.pack()
+            frame.setLocationRelativeTo(null)
+            frame.isVisible = true
+        }
+    }
+}
