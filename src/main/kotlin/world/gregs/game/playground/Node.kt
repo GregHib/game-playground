ddd02a6b1db63da83fb05892dd3e437a600b3733
Greg <gregoryhib@gmail.com>
2020-02-15 11:46:02 +0000

    More A* stuff

--- src/main/kotlin/world/gregs/game/playground/Node.kt
+++ src/main/kotlin/world/gregs/game/playground/Node.kt
@@ -4,6 +4,8 @@ import kotlin.math.abs
 
 open class Node(val x: Int, val y: Int) {
 
+    val pos = y + (x shl 14)// + (plane shl 28)
+
     fun delta(v: Node): Node {
         return Node(abs(this.x - v.x), abs(this.y - v.y))
     }
--- src/main/kotlin/world/gregs/game/playground/pathfinding/jps/JPSFinderTest.kt
+++ src/main/kotlin/world/gregs/game/playground/pathfinding/jps/JPSFinderTest.kt
@@ -1,8 +1,12 @@
 package world.gregs.game.playground.pathfinding.jps
 
 import world.gregs.game.playground.Grid
+import world.gregs.game.playground.euclidean
+import world.gregs.game.playground.manhattan
+import world.gregs.game.playground.pathfinding.astar.impl2.AStar2
 import world.gregs.game.playground.pathfinding.jps.node.JPSNode2
 import world.gregs.game.playground.pathfinding.jps.node.NodeState
+import world.gregs.game.playground.ui.zoom.SolidGrid
 
 
 object JPSFinderTest {
@@ -10,34 +14,51 @@ object JPSFinderTest {
     @JvmStatic
     fun main(args: Array<String>) {
 
-        val grid = Grid<JPSNode2>(8, 8)
+        val grid = object : Grid<JPSNode2>(6, 6), SolidGrid {
+            override fun blocked(x: Int, y: Int): Boolean {
+                return get(x, y)?.state == NodeState.WALL
+            }
+        }
         for (x in grid.colIndices) {
             for (y in grid.rowIndices) {
                 grid.set(x, y, JPSNode2(x, y))
             }
         }
 
-        grid.get(1, 6)?.state = NodeState.WALL
-        grid.get(2, 6)?.state = NodeState.WALL
-        grid.get(3, 6)?.state = NodeState.WALL
-        grid.get(4, 6)?.state = NodeState.WALL
-        val start = grid.get(1, 1)!!
-        val end = grid.get(2, 7)!!
+        grid.get(1, 1)!!.state = NodeState.WALL
+        grid.get(2, 1)?.state = NodeState.WALL
+        grid.get(3, 1)?.state = NodeState.WALL
+        grid.get(4, 1)?.state = NodeState.WALL
+        grid.get(5, 1)?.state = NodeState.WALL
+        grid.get(1, 2)?.state = NodeState.WALL
+        grid.get(1, 3)?.state = NodeState.WALL
+        grid.get(2, 3)?.state = NodeState.WALL
+//        start.state = NodeState.EMPTY
+//        end.state = NodeState.EMPTY
+        for (y in grid.rowIndices.reversed()) {
+            for (x in grid.colIndices) {
+//                println("$x, $y ${grid.get(x, y)?.state}")
+                print("${if(grid.get(x, y)?.state == NodeState.WALL) 1 else 0} ")
+            }
+            println()
+        }
+        val start = grid.get(5, 0)!!
+        val end = grid.get(5, 2)!!
 
         val f = JPSFinder()
 
-//        val r: Result
-//        try {
-//            r = f.findPath(grid, start, end, euclidean, 1.0)
-//
-//            for (v in r.path()) {
-//                System.out.println(v)
-//            }
-//
-//            println(String.format("%d ms %d ops", r.duration(), r.operations()))
-//        } catch (e: InterruptedException) {
-//            e.printStackTrace()
-//        }
+        val r: Result
+        try {
+            r = f.findPath(grid, start, end, AStar2.manhattan, 1.0)
+
+            for (v in r.path()) {
+                println(v)
+            }
+
+            println(String.format("%d ms %d ops", r.duration(), r.operations()))
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
 
 
     }
