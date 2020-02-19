4d8470e59fd318a1c09a66603426160dd96deb67
GregHib <gregoryhib@gmail.com>
2020-02-19 13:17:58 +0000

    Enhanced HPA* WIP

--- src/main/kotlin/world/gregs/game/playground/pathfinding/bfs/BreadthFirstSearch.kt
+++ src/main/kotlin/world/gregs/game/playground/pathfinding/bfs/BreadthFirstSearch.kt
@@ -8,6 +8,7 @@ import world.gregs.game.playground.Grid
 import world.gregs.game.playground.Node
 import world.gregs.game.playground.ui.zoom.GridCanvas
 import world.gregs.game.playground.ui.zoom.SolidGrid
+import java.awt.Rectangle
 import java.util.*
 
 class BreadthFirstSearch(private val directions: Array<Direction>) {
@@ -46,5 +47,47 @@ class BreadthFirstSearch(private val directions: Array<Direction>) {
             }
         }
         return distances
+    }
+    fun search(grid: Grid<*>, start: Node, bounds: Rectangle): List<Node> {
+        val distances = Array(grid.columns) { Array(grid.rows) { -1 } }
+        val list = mutableListOf<Node>()
+        val queue = LinkedList<Node>()
+        queue.add(start)
+        list.add(start)
+        distances[start.x][start.y] = 0
+        while (queue.isNotEmpty()) {
+            val parent = queue.poll()
+            directions.forEach { direction ->
+                val x = parent.x + direction.x
+                val y = parent.y + direction.y
+                if(bounds.contains(x, y) &&  !(grid as SolidGrid).blocked(x, y) && distances[x][y] == -1) {
+                    val distance = distances[parent.x][parent.y] + 1
+                    distances[x][y] = distance
+                    queue.add(Node(x, y))
+                    list.add(Node(x, y))
+                }
+            }
+        }
+        return list
+    }
+    fun forEach(grid: Grid<*>, start: Node, bounds: Rectangle, predicate: (Int, Int, Int) -> Unit) {
+        val distances = Array(grid.columns) { Array(grid.rows) { -1 } }
+        val queue = LinkedList<Node>()
+        queue.add(start)
+        distances[start.x][start.y] = 0
+        while (queue.isNotEmpty()) {
+            val parent = queue.poll()
+            directions.forEach { direction ->
+                val x = parent.x + direction.x
+                val y = parent.y + direction.y
+                if(grid.inBounds(x, y) && bounds.contains(x, y) && !(grid as SolidGrid).blocked(x, y) && distances[x][y] == -1) {
+                    val distance = distances[parent.x][parent.y] + 1
+                    distances[x][y] = distance
+                    val node = Node(x, y)
+                    queue.add(node)
+                    predicate(x, y, distance)
+                }
+            }
+        }
     }
 }
